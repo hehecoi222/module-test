62b0f1c9dd1d1b317bb45e611000d2b9cdac38fa
hehecoi222 <hehecoi@outlook.com>
2021-09-18 00:40:38 +0700

    Hello: initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hehecoi222/module-test
+
+go 1.17
---
+++ module-test.go
@@ -0,0 +1,9 @@
+// To test go module in other repository
+package modulecheck
+
+import "fmt"
+
+// ModPrint is used to print "You're succeed"
+func ModPrint() {
+	fmt.Println("You're succeed")
+}
