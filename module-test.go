5193106b565b78c48d000e773bd5e1afde03b5d4
hehecoi222 <hehecoi@outlook.com>
2021-09-18 17:52:09 +0700

    Alright

--- module-test.go
+++ module-test.go
@@ -1,4 +1,10 @@
 // To test go module in other repository
+
+//Yes we go on Beyond with v2: v2 changelog
+/*
+Nothing much, just test
+*/
+
 package modulecheck
 
 import "fmt"
@@ -7,4 +13,11 @@ import "fmt"
 func ModPrint() {
 	fmt.Println("You're succeed")
 	fmt.Println("Now Update")
+}
+
+// GoPrint is used to print We're gophers
+// 
+//		with the addition about to publish
+func GoPrint()  {
+	fmt.Println("We're Gophers and about to publish")
 }
