8fb4398e47ba18a0ff56fedb9d68190e5945bc42
hehecoi222 <hehecoi@outlook.com>
2021-09-20 15:50:58 +0700

    Add exxample to demonstarte code

---
+++ module-test_test.go
@@ -0,0 +1,11 @@
+package modulecheck_test
+
+import modulecheck "github.com/hehecoi222/module-test"
+
+func ExampleModPrint() {
+	modulecheck.ModPrint()
+	
+	//Output:
+	//You're succeed
+	//Now Update
+}
